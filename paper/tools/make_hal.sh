171cd456d58fdf387a45591a99ff1525fd8d6b9f
Arthur Mensch <arthur.mensch@gmail.com>
2020-06-16 14:41:08 +0200

    preprint v2

--- paper/tools/make_hal.sh
+++ paper/tools/make_hal.sh
@@ -6,7 +6,7 @@ mkdir hal
 ls | grep -v hal | xargs -I {} bash -c "cp -r {} hal"
 cd hal
 docker run --rm --volume $PWD:/hal --workdir /hal makisyu/texlive-2016 make article.pdf
-rm -f article-blx.bib article.aux article.bib article.blg article.fdb_latexmk article.fls article.log article.out article.pdf article.run.xml article.synctex.gz Makefile biblio.bib
+rm -rf article.aux article.blg article.fdb_latexmk article.fls article.log article.out article.pdf article.run.xml article.synctex.gz Makefile code tools icml* neurips*
 zip -r hal.zip *
 cp hal.zip ..
 cd ..
